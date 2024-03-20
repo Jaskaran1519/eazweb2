aabcfb825ccac4bf8dc4e1343522839c4586c237
Jaskaran1519 <jaskaran1519@gmail.com>
2024-03-20 17:02:11 +0530

    hey nigg

--- app/page.js
+++ app/page.js
@@ -2,7 +2,6 @@ import React from "react";
 import Hero from "./_components/Hero";
 import Header from "./_components/Header";
 import Service from "./_components/Service";
-import ProjectCard from "./_components/ProjectCard";
 import Review from "./_components/Review";
 
 const page = () => {
@@ -11,26 +10,6 @@ const page = () => {
       <Header />
       <Hero />
       <Service />
-      <div className="w-[80%] mx-auto mt-5">
-        <h1 className="text-[2rem] font-semibold">Some of our work</h1>
-        <div className=" mx-auto grid grid-cols-3 gap-4">
-          <ProjectCard
-            imageUrl="/next.svg"
-            title="Project 1"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-          />
-          <ProjectCard
-            imageUrl="/enw.svg"
-            title="Project 2"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-          />
-          <ProjectCard
-            imageUrl="/rightbox.jpg"
-            title="Project 3"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-          />
-        </div>
-      </div>
       <Review />
     </div>
   );
