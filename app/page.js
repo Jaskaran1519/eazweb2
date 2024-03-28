c8493e8377512d57b36859b09336be029da4781e
Jaskaran1519 <jaskaran1519@gmail.com>
2024-03-28 19:07:52 +0530

    ok

--- app/page.js
+++ app/page.js
@@ -1,12 +1,27 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import Hero from "./_components/Hero";
 import Header from "./_components/Header";
 import Paragraph from "./_components/Paragraph";
 import Stats from "./_components/Stats";
-
+import Lenis from "@studio-freight/lenis";
 import Review from "./_components/Review";
 
 const page = () => {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    lenis.on("scroll", (e) => {
+      console.log(e);
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  });
   return (
     <div className="w-[90%] mx-auto z-0 ">
       <div className="sticky top-0 z-50">
