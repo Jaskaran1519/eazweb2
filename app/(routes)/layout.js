0fff4ab6ccaa714254b01c59c9cdf815668683cb
Jaskaran1519 <jaskaran1519@gmail.com>
2024-06-19 13:36:10 +0530

    okl

--- app/(routes)/layout.js
+++ app/(routes)/layout.js
@@ -1,7 +1,23 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import Header from "../_components/Header";
+import Lenis from "@studio-freight/lenis";
 
 const layout = ({ children }) => {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    lenis.on("scroll", (e) => {
+      console.log(e);
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  });
   return (
     <div className="w-[90%] mx-auto">
       <div className="sticky top-0 z-50">
